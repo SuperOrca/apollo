e18ab31fb8f1463282d0c41a9ec1072a26d404e7
SuperOrca <70454918+SuperOrca@users.noreply.github.com>
2021-07-09 19:03:14 -0400

    fix help 4

--- utils/help.py
+++ utils/help.py
@@ -5,17 +5,17 @@ from discord.ext import commands
 class ApolloHelp(commands.HelpCommand):
     async def send_bot_help(self, mapping: dict):
         embed = discord.Embed(title="Apollo Help", description=f"""
-        Total Commands: `{len(self.context.bot.commands)}`
-        ```diff
-        - <> | Required Argument
-        - [] | Optional Argument
-        + Type >help [Command | Module] for more info on a command and/or module!
-        ```
-        `@Apollo invite` | `@Apollo info`
-        """, color=discord.Color.blurple())
+Total Commands: `{len(self.context.bot.commands)}`
+```diff
+- <> | Required Argument
+- [] | Optional Argument
++ Type >help [Command | Module] for more info on a command and/or module!
+```
+`?invite` | `?info`
+""", color=discord.Color.blurple())
         embed.add_field(
             name=":gear: Modules",
-            value='\n'.join(module.__class__.__name__ for module in mapping),
+            value='\n'.join(f"- `{module.__class__.__name__}`" for module in mapping),
             inline=True,
         )
         embed.add_field(name=":newspaper: News", value="Invite my bot to ur server pls :)")
