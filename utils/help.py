0de775ccf9c924dff1b92e8239066f9a38e0def6
SuperOrca <70454918+SuperOrca@users.noreply.github.com>
2021-07-10 13:17:04 -0400

    Update help.py

--- utils/help.py
+++ utils/help.py
@@ -31,7 +31,8 @@ Total Commands: `{len(valid_commands)}`
     async def send_command_help(self, command: commands.Command):
         embed = discord.Embed(
             title=f"**`{command.usage if command.usage is not None else command.name}`**", description=command.description, color=discord.Color.blurple())
-        embed.add_field(name="Module", value=command.module)
+        embed.add_field(
+            name="Module", value=command.module.split('.')[1].capitalize())
         if command.aliases != []:
             embed.add_field(name=f"Aliases [{len(command.aliases)}]", value='\n'.join(
                 f'- `{aliase}`' for aliase in command.aliases))
