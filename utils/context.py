c16ef57c93efb61aa73454f7a701e2d1fdbfa5f3
SuperOrca <70454918+SuperOrca@users.noreply.github.com>
2021-07-11 15:30:18 -0400

    fix ctx.trash

--- utils/context.py
+++ utils/context.py
@@ -9,13 +9,14 @@ class Context(commands.Context):
 
     async def trash(self, content: str = None, **kwargs):
         class TrashView(ui.View):
-            def __init__(self):
+            def __init__(self, author):
                 super().__init__()
+                self.author = author
 
-            @ui.button(label='🗑', style=discord.ButtonStyle.red)
-            async def previous(self, button: ui.Button, interaction: discord.Interaction):
+            @ui.button(label='🗑️', style=discord.ButtonStyle.red)
+            async def delete(self, button: ui.Button, interaction: discord.Interaction):
                 if self.author == interaction.user:
                     await interaction.message.delete()
                 else:
                     await interaction.response.send_message("This is not your command.", ephemeral=True)
-        return await self.message.reply(content, **kwargs, mention_author=False, view=TrashView())
+        return await self.message.reply(content, **kwargs, mention_author=False, view=TrashView(self.author))
