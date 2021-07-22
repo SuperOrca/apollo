ba1a23c9a95a672a23f0ce6594be3009736b941c
SuperOrca <70454918+SuperOrca@users.noreply.github.com>
2021-07-22 14:55:49 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -156,7 +156,7 @@ class Apollo(commands.AutoShardedBot):
             return
         if message.content.startswith('jsk') and message.author.id == int(getenv('OWNER_ID')):
             message.content = self.user.mention + " " + message.content
-        if message.content.strip() in commands.when_mentioned(self, message):
+        if message.content in [men.strip() for men in commands.when_mentioned(self, message)]:
             return await message.reply(f"The server prefix is `{await self.get_guild_prefix(message)}`.")
         await self.process_commands(message)
 
