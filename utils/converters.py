ac950dd0df9dcc61f28213c8750d52d24f982f22
SuperOrca <70454918+SuperOrca@users.noreply.github.com>
2021-07-26 15:30:56 -0400

    fix converter mayb pog?

--- utils/converters.py
+++ utils/converters.py
@@ -1,10 +1,14 @@
+from io import BytesIO
 import discord
 from discord.ext import commands
+import twemoji_parser as twemoji
 
+
+import re
 from typing import Union
 
 from .context import ApolloContext
-from .image import imageToBytes
+from .metrics import isImage
 
 
 class PrefixConverter(commands.clean_content):
@@ -21,9 +25,58 @@ class PrefixConverter(commands.clean_content):
             raise commands.BadArgument(
                 'Your prefix can not be more than 15 characters.')
 
-        return argument
+        url = argument
 
 
 class ImageConverter(commands.Converter):
-    async def convert(self, ctx: ApolloContext, image: Union[discord.Member, discord.Emoji, discord.PartialEmoji, None, str] = None):
-        return imageToBytes(ctx, image)
+    async def convert(self, ctx: ApolloContext, url: Union[discord.Member, discord.Emoji, discord.PartialEmoji, None, str] = None) -> BytesIO:
+        if isinstance(url, str):
+            url = await twemoji.emoji_to_url(url)
+
+        if ctx.message.reference:
+            ref = ctx.message.reference.resolved
+            if ref.embeds:
+                if ref.embeds[0].image.url != discord.Embed.Empty and isImage(
+                        ref.embeds[0].image.url
+                ):
+                    url = ref.embeds[0].image.url
+
+                if ref.embeds[0].thumbnail.url != discord.Embed.Empty and isImage(
+                        ref.embeds[0].thumbnail.url
+                ):
+                    url = ref.embeds[0].thumbnail.url
+
+            elif ref.attachments:
+                url = ref.attachments[0].url or ref.attachments[0].proxy_url
+                if isImage(url):
+                    url = url
+
+        if isinstance(url, discord.Member):
+            url = str(url.avatar.url)
+        elif isinstance(url, str):
+            if re.search(
+                    r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
+                    url,
+            ) and isImage(url):
+                url = url
+
+        if isinstance(url, (discord.Emoji, discord.PartialEmoji)):
+            url = url.url
+
+        if ctx.message.attachments:
+
+            url = ctx.message.attachments[0].url or ctx.message.attachments[0].proxy_url
+
+            if isImage(url):
+                url = ctx.message.attachments[0].proxy_url or ctx.message.attachments[0].url
+
+            elif isinstance(url, discord.Member):
+                url = str(url.avatar.url)
+            else:
+                url = str(ctx.author.avatar.url)
+
+        if url is None:
+            url = str(ctx.author.avatar.url)
+
+        response = await ctx.bot.session.get(url)
+        return BytesIO(await response.read())
