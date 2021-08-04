fce452ad8dc9af683db57049425b7b63dc6a8998
SuperOrca <70454918+SuperOrca@users.noreply.github.com>
2021-08-04 17:30:02 -0400

    Update image.py

--- utils/image.py
+++ utils/image.py
@@ -31,10 +31,13 @@ async def wand_process(ctx: ApolloContext, image: AssetResponse, operation) -> N
 	blob = await url_to_bytes(ctx, image.url)
 	if image.is_animated():
 		_format = 'gif'
-		with Image(blob=blob) as new:
-			for frame in new.sequence:
+		base = Image(blob=blob)
+		with Image() as new:
+			for frame in base.sequence:
 				operation(frame)
+				new.sequence.append(frame)
 			buffer = new.make_blob(format=_format)
+		base.close()
 	else:
 		_format = 'png'
 		with Image(blob=blob) as new:
