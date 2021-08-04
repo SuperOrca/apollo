622efced0421aba56bedb10d9f458b70c387b659
SuperOrca <70454918+SuperOrca@users.noreply.github.com>
2021-08-04 12:52:01 -0400

    move eight ball to config

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,4 +1,5 @@
 import asyncio
+from config import EIGHT_BALL
 from random import choice, randint
 from typing import Optional
 
@@ -6,30 +7,6 @@ from discord.ext import commands
 
 from utils.context import ApolloContext
 from utils.metrics import Embed
-
-_8ball_responses = (
-    "It is certain",
-    "It is decidedly so",
-    "Without a doubt",
-    "Yes, definitely",
-    "You may rely on it",
-    "As I see it, yes",
-    "Most likely",
-    "Outlook good",
-    "Yes",
-    "Signs point to yes",
-    "Reply hazy try again",
-    "Ask again later",
-    "Better not tell you now",
-    "Cannot predict now",
-    "Concentrate and ask again",
-    "Don't count on it",
-    "My reply is no",
-    "My sources say no",
-    "Outlook not so good",
-    "Very doubtful"
-)
-
 
 class Fun(commands.Cog):
     def __init__(self, bot) -> None:
@@ -49,7 +26,7 @@ class Fun(commands.Cog):
 
     @commands.command(name='8ball', description="Answers a yes/no question.", usage="<question>")
     async def _8ball(self, ctx: ApolloContext, *, question: commands.clean_content) -> None:
-        await ctx.reply(embed=Embed(description=f"**Question**: {question}\n**Answer**: {choice(_8ball_responses)}"))
+        await ctx.reply(embed=Embed(description=f"**Question**: {question}\n**Answer**: {choice(EIGHT_BALL)}"))
 
     @commands.command(name='pp', description="Shows pp size of member.", usage="[member]")
     async def _pp(self, ctx: ApolloContext, member: Optional[commands.UserConverter] = None):
--- config.py
+++ config.py
@@ -19,13 +19,13 @@ jotte sus
 """
 
 INTENTS = discord.Intents(
-    guilds=True,
-    members=True,
-    bans=True,
-    emojis=True,
-    voice_states=True,
-    messages=True,
-    reactions=True
+	guilds=True,
+	members=True,
+	bans=True,
+	emojis=True,
+	voice_states=True,
+	messages=True,
+	reactions=True
 )
 
 MENTIONS = discord.AllowedMentions.none()
@@ -33,3 +33,26 @@ MENTIONS = discord.AllowedMentions.none()
 STRIP_AFTER_PREFIX = True
 CASE_INSENSITIVE = True
 MAX_MESSAGES = 10000
+
+EIGHT_BALL = (
+	"It is certain",
+	"It is decidedly so",
+	"Without a doubt",
+	"Yes, definitely",
+	"You may rely on it",
+	"As I see it, yes",
+	"Most likely",
+	"Outlook good",
+	"Yes",
+	"Signs point to yes",
+	"Reply hazy try again",
+	"Ask again later",
+	"Better not tell you now",
+	"Cannot predict now",
+	"Concentrate and ask again",
+	"Don't count on it",
+	"My reply is no",
+	"My sources say no",
+	"Outlook not so good",
+	"Very doubtful"
+)
