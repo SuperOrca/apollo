177546f8542b91329860b9db567c426a96a60dac
SuperOrca <70454918+SuperOrca@users.noreply.github.com>
2021-08-04 19:34:35 -0400

    Update dagpi.py

--- cogs/dagpi.py
+++ cogs/dagpi.py
@@ -1,5 +1,6 @@
 from inspect import Parameter
 from typing import Optional
+from asyncio import AbstractEventLoop
 
 from discord.ext import commands
 
@@ -15,10 +16,10 @@ def _transform(self, ctx, param):
         loop = ctx.bot.loop
         if ctx.message.attachments:
             param = Parameter(
-                param.name, param.kind, default=loop.run_until_complete(ImageConverter().convert(ctx, ctx.message.attachments[0].url)), annotation=ImageConverter)
+                param.name, param.kind, default=loop.create_task(ImageConverter().convert(ctx, ctx.message.attachments[0].url)).result(), annotation=ImageConverter)
         else:
             param = Parameter(
-                param.name, param.kind, default=loop.run_until_complete(ImageConverter().convert(ctx, ctx.author.avatar.url)), annotation=ImageConverter)
+                param.name, param.kind, default=loop.create_task(ImageConverter().convert(ctx, ctx.author.avatar.url)).result(), annotation=ImageConverter)
 
     return _old_transform(self, ctx, param)
 
